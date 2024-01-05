185690d78e8f8ad8eeb640751f1745a62058bac9
VASTVIKTA NISHAD <96430679+VMONSTER7@users.noreply.github.com>
2024-01-05 12:44:54 +0530

    Given head, the head of a linked list, determine if the linked list has a cycle in it.

---
+++ hasCycle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+    ListNode* fast=head;
+    ListNode* slow=head;
+
+    while(fast && fast->next!=nullptr)
+    {
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast)return true;
+    }
+    return false;
+    }
+
+};
